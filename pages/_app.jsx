da9d13cd9fcc663210118780138380a1974ba399
davidmul99snl <davidmul99@hotmail.com>
2025-08-10 16:46:12 +0100

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import SiteHeader from '../components/SiteHeader'
+import SiteBrand from '../components/SiteBrand'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <SiteHeader />
+      <SiteBrand />
       <Component {...pageProps} />
     </>
   )
