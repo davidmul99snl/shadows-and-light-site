973a86eee6cfdbdf4dd4b62f9d8fd1f6931b5bc2
davidmul99snl <davidmul99@hotmail.com>
2025-08-10 17:27:23 +0100

    Update press.jsx

--- pages/press.jsx
+++ pages/press.jsx
@@ -4,21 +4,9 @@ import styles from '../styles/Press.module.css'
 const HERO = '/hero-press.jpg'
 
 const QUOTES = [
-  {
-    quote:
-      "An unquestionable highlight of this year’s Out to Lunch was the performance of Joni Mitchell’s material by Dublin based Shadows and Light. A sold out Black Box was held enthralled for over two hours...",
-    cite: "Sean Kelly, Director of the Out to Lunch Festival, Belfast",
-  },
-  {
-    quote:
-      "If you are a Joni Mitchell fan, you will love Shadows and Light... great musicianship and vocals to die for!",
-    cite: "Damien Murray – Belfast Telegraph, U105 and Belfast 89FM",
-  },
-  {
-    quote:
-      "Amazing. Our music highlight of the year so far. Bring them back straight away",
-    cite: "Audience member, Culture Night Belfast",
-  },
+  { quote: "An unquestionable highlight of this year’s Out to Lunch was the performance of Joni Mitchell’s material by Dublin based Shadows and Light. A sold out Black Box was held enthralled for over two hours...", cite: "Sean Kelly, Director of the Out to Lunch Festival, Belfast" },
+  { quote: "If you are a Joni Mitchell fan, you will love Shadows and Light... great musicianship and vocals to die for!", cite: "Damien Murray – Belfast Telegraph, U105 and Belfast 89FM" },
+  { quote: "Amazing. Our music highlight of the year so far. Bring them back straight away", cite: "Audience member, Culture Night Belfast" },
 ];
 
 export default function Press() {
@@ -28,9 +16,9 @@ export default function Press() {
         <div className={styles.scrim} />
       </div>
 
-      <div className={styles.grid}>
-        <h1 className={styles.pageTitle}>Press</h1>
+      <h1 className={styles.pageTitle}>Press</h1>
 
+      <div className={styles.grid}>
         {QUOTES.map((q, i) => (
           <blockquote className={styles.card} key={i}>
             <p>“{q.quote}”</p>
